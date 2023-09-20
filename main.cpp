bf0c50adc701d2b55b7d8bd09df68d9e2e6fa37d
fodesu <himeno.senaa@gmail.com>
2023-09-20 10:00:19 +0800

    init

--- main.cpp
+++ main.cpp
@@ -1,14 +1,16 @@
 #include <QApplication>
 #include <QPushButton>
-#include <iostream>
+
+#include "MainWindow.h"
+
 
 int main(int argc, char *argv[]) {
     QApplication a(argc, argv);
+    MainWindow b;
+    b.resize(800, 600);
+    b.show();
     QPushButton button("Hello world!", nullptr);
     button.resize(200, 100);
     button.show();
-
-
-    std::cout << "test" << std::endl;
     return QApplication::exec();
 }
